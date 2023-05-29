1d89c679f321481b44c4396cebef916d727017c6
Silas Buarque <silasbuarque@gmail.com>
2023-05-28 21:17:59 -0300

    Initial Commit

---
+++ src/main/java/br/com/secretpasswordstorege/secretpasswordstorege/SecretPasswordStoregeApplication.java
@@ -0,0 +1,13 @@
+package br.com.secretpasswordstorege.secretpasswordstorege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretPasswordStoregeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretPasswordStoregeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/secretpasswordstorege/secretpasswordstorege/SecretPasswordStoregeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.secretpasswordstorege.secretpasswordstorege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretPasswordStoregeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
